ec131ae81a94deb12f2a603ac2e107660738b972
ibanez2du38 <ibanez2@hotmail.fr>
2021-08-26 10:36:49 +0200

    build (on branch structure) : add structure html on header and footer.php and require necessary on index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'asset' . DIRECTORY_SEPARATOR . 'header.php';
+?>
+
+<?php
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'asset' . DIRECTORY_SEPARATOR . 'footer.php';
+?>
